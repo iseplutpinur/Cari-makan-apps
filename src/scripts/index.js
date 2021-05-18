b3ca1fbb1c10be1930a4a862223e193b4c0a405c
Isep Lutpi Nur <iseplutpinur7@gmail.com>
2021-05-18 21:30:48 +0700

    Finish app bar nav

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,22 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 
+const menu = document.querySelector('#menu');
+// const hero = document.querySelector('.hero');
+const main = document.querySelector('main');
+const drawer = document.querySelector('#nav_drawer');
+
+menu.addEventListener('click', function (event) {
+    drawer.classList.toggle('open');
+    event.stopPropagation();
+});
+
+// hero.addEventListener('click', function () {
+//     drawer.classList.remove('open');
+// });
+
+main.addEventListener('click', function () {
+    drawer.classList.remove('open');
+});
+
 console.log('Hello Coders! :)');
